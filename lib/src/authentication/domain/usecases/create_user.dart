b4a7057f0022e720b0dbbafd827bdfec34f85782
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2024-07-23 12:01:24 -0300

    Domain layer usecases

---
+++ lib/src/authentication/domain/usecases/create_user.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+import 'package:tdd_tutorial/core/usecase/usecase.dart';
+import 'package:tdd_tutorial/core/utils/typedef.dart';
+import 'package:tdd_tutorial/src/authentication/domain/repositories/authentication_repository.dart';
+
+class CreateUser extends UsecaseWithParams<void, CreateUserParams> {
+  final AuthenticationRepository _repository;
+
+  CreateUser(this._repository);
+
+  @override
+  ResultFuture<void> call(CreateUserParams params) async {
+    return _repository.createUser(createdAt: params.createdAt, name: params.name, avatar: params.avatar);
+  }
+}
+
+class CreateUserParams extends Equatable {
+  final String createdAt;
+  final String name;
+  final String avatar;
+
+  const CreateUserParams({required this.createdAt, required this.name, required this.avatar});
+
+  @override
+  List<Object?> get props => [createdAt, name, avatar];
+}
